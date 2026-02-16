8fc99f29523de3d906911dd6c1fe09cd2640952b
zzuse <zzuse@outlook.com>
2026-02-16 04:10:35 -0600

    dotfile re-arch

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,113 +1,96 @@
-#!/bin/sh
-# Check for Homebrew,
-# Install if we don't have it
-if test ! $(which brew); then
-	echo "Installing homebrew..."
-	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#!/bin/bash
+
+# Detect macOS version
+MACOS_VERSION=$(sw_vers -productVersion | cut -d. -f1)
+THRESHOLD=14
+
+if [ "$MACOS_VERSION" -lt "$THRESHOLD" ]; then
+    PM="port"
+else
+    PM="brew"
 fi
 
-chsh -s $(which zsh)
-# Update homebrew recipes
-# FAQ1:sudo chown -R $(whoami):admin /usr/local/Homebrew/.git
-echo "updating brew..."
-#brew update
+echo "Detected macOS version $MACOS_VERSION. Using $PM for package management."
 
-echo "before install with brew, you can refer to Brewfile"
+xcode-select --install
+# Install Package Manager if missing
+if [ "$PM" == "brew" ]; then
+    if ! command -v brew >/dev/null 2>&1; then
+        echo "Installing Homebrew..."
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
+    echo "Updating Homebrew..."
+    brew update
+elif [ "$PM" == "port" ]; then
+    if ! command -v port >/dev/null 2>&1; then
+        echo "MacPorts not found. Please install it from https://www.macports.org/install.php"
+        exit 1
+    fi
+    echo "Updating MacPorts..."
+    sudo port selfupdate
+fi
 
-#taps
+# Change shell to zsh if not already
+if [[ "$SHELL" != */zsh ]]; then
+    echo "Changing shell to zsh..."
+    chsh -s "$(which zsh)"
+fi
 
-taps=(
-	thoughtbot/formulae
-)
-echo "installing binaries..."
-echo "browse from http://braumeister.org"
-brew tap ${taps[@]}
-
-brew cleanup
-
+# Package Lists
 binaries=(
-#	cntlm
-#	htop
-	rcm
-#	mackup
-	macvim
-#	"wget --with-iri"
-	tree
-	socat
-	node
-	nmap
-#	e2fsprogs
-#	neofetch
-#	"reattach-to-user-namespace --with-wrap-pbcopy-and-pbpaste"
-	zsh
-	zsh-completions
-	tmux
-#	graphviz
-#	bazel
-	cmake
-	clang-format
-    telnet
+    rcm
+    tree
+    socat
+    nmap
+    zsh
+    zsh-completions
+    tmux
+    cmake
+    clang-format
 )
 
-echo "installing binaries..."
-brew install ${binaries[@]}
-
-brew cleanup
-
-# Apps
 apps=(
-    appcleaner
-    #evernote
-    #google-chrome
-    #qq
-    qqmusic
-    #neteasemusic
-    #skitch
-    #vlc
-    #mplayerx
-    keycastr
-    #beyond-compare
-    #aliwangwang
-    #calibre
-    #coderunner
-    #filezilla
-    #geektool
-    gitup
-    #hex-fiend
-    #keepassx
-    #nutstore
-    #shadowsocksx
-    #staruml
-    #torbrowser
-    #virtualbox
     vnc-viewer
-    #baidunetdisk
-    #postman
-    #wireshark
-    #dukto
-    #mysqlworkbench
-    #pycharm-ce
-    #visual-studio-code
-    #hammerspoon
-    #downie
-    #sourcetree
-    #spectacle
-    #amethyst
-    #android-platform-tools
 )
 
-# Install apps to /Applications
-# Default is: /Users/$user/Applications
-echo "installing apps..."
-echo "browse https://caskroom.github.io/search"
-brew cask install ${apps[@]}
-brew cask install caskroom/versions/java8
+# Installation Logic
+if [ "$PM" == "brew" ]; then
+    echo "Installing binaries via Homebrew..."
+    brew install "${binaries[@]}"
 
-echo "installing dotfiles..."
-export RCRC="$HOME/dotfiles/rcrc"
-rcup
+    echo "Installing apps via Homebrew Cask..."
+    brew install --cask "${apps[@]}"
+elif [ "$PM" == "port" ]; then
+    echo "Installing binaries via MacPorts..."
+    for pkg in "${binaries[@]}"; do
+        case "$pkg" in
+            telnet) port_pkg="inetutils" ;;
+            *) port_pkg="$pkg" ;;
+        esac
+        echo "Installing $port_pkg..."
+        sudo port install "$port_pkg"
+    done
+    
+    echo "Note: MacPorts does not support Cask-like app installations for all GUI apps."
+    echo "Please install these manually if needed: ${apps[*]}"
+fi
 
-echo "installing dotfiles-oschina..."
-export RCRC="$HOME/dotfiles-oschina/rcrc"
-rcup
+# Dotfiles installation
+echo "Installing dotfiles..."
+if command -v rcup >/dev/null 2>&1; then
+    export RCRC="$HOME/dotfiles/rcrc"
+    rcup -f
+    
+    if [ -d "$HOME/dotfiles-oschina" ]; then
+        echo "Installing dotfiles-oschina..."
+        export RCRC="$HOME/dotfiles-oschina/rcrc"
+        rcup -f
+    fi
+else
+    echo "rcup not found. Please ensure rcm is installed correctly."
+fi
 
+echo "Cleanup..."
+if [ "$PM" == "brew" ]; then
+    brew cleanup
+fi
--- zshrc
+++ zshrc
@@ -11,49 +11,6 @@ export ZSH=$HOME/.oh-my-zsh
 #ZSH_THEME="random"
 ZSH_THEME="half-life"
 
-# Uncomment the following line to use case-sensitive completion.
-# CASE_SENSITIVE="true"
-
-# Uncomment the following line to use hyphen-insensitive completion. Case
-# sensitive completion must be off. _ and - will be interchangeable.
-# HYPHEN_INSENSITIVE="true"
-
-# Uncomment the following line to disable bi-weekly auto-update checks.
-# DISABLE_AUTO_UPDATE="true"
-
-# Uncomment the following line to change how often to auto-update (in days).
-# export UPDATE_ZSH_DAYS=13
-
-# Uncomment the following line to disable colors in ls.
-# DISABLE_LS_COLORS="true"
-
-# Uncomment the following line to disable auto-setting terminal title.
-# DISABLE_AUTO_TITLE="true"
-
-# Uncomment the following line to enable command auto-correction.
-# ENABLE_CORRECTION="true"
-
-# Uncomment the following line to display red dots whilst waiting for completion.
-# COMPLETION_WAITING_DOTS="true"
-
-# Uncomment the following line if you want to disable marking untracked files
-# under VCS as dirty. This makes repository status check for large repositories
-# much, much faster.
-# DISABLE_UNTRACKED_FILES_DIRTY="true"
-
-# Uncomment the following line if you want to change the command execution time
-# stamp shown in the history command output.
-# The optional three formats: "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
-# HIST_STAMPS="mm/dd/yyyy"
-
-# Would you like to use another custom folder than $ZSH/custom?
-# ZSH_CUSTOM=/path/to/new-custom-folder
-
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
-# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
-# Example format: plugins=(rails git textmate ruby lighthouse)
-# Add wisely, as too many plugins slow down shell startup.
-
 source ~/dotfiles/func/notify-send
 plugins=(git ruby vi-mode last-working-dir zsh-autosuggestions themes zsh-syntax-highlighting auto-notify)
 
@@ -96,18 +53,6 @@ export AUTO_NOTIFY_BODY="It completed in %elapsed seconds with exit code %exit_c
 # }
 # export rm
 
-#OS specific
-#case `uname` in
-#  Darwin)
-#    source "${HOME}/.zshrc-Darwin"
-#  ;;
-#  Linux)
-#    source "${HOME}/.zshrc-Linux"
-#  ;;
-#  FreeBSD)
-#    source "${HOME}/.zshrc-`uname`"
-#  ;;
-#esac
 if [ -f "${HOME}/.zshrc-`uname`" ]; then
     source "${HOME}/.zshrc-`uname`"
 fi
@@ -159,28 +104,6 @@ printf "%s         \/%s      \/%s       %s     \/ %s     \/ %s\n" $RB_RED $RB_OR
 printf "%s*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*%s\n" $RB_VIOLET ${BOLD} $RB_RESET
 printf "%s%sctrl+shift+Q send exit in iTerm2%s\n" $RB_INDIGO ${BOLD} $RB_RESET
 printf "%s%sctrl+shift+option+left send topleft in phoenix%s\n" $RB_INDIGO ${BOLD} $RB_RESET
-
-# export MANPATH="/usr/local/man:$MANPATH"
-
-# You may need to manually set your language environment
-# export LANG=en_US.UTF-8
-
-# Preferred editor for local and remote sessions
-
-# Compilation flags
-# export ARCHFLAGS="-arch x86_64"
-
-# ssh
-# export SSH_KEY_PATH="~/.ssh/dsa_id"
-
-# Set personal aliases, overriding those provided by oh-my-zsh libs,
-# plugins, and themes. Aliases can be placed here, though oh-my-zsh
-# users are encouraged to define aliases within the ZSH_CUSTOM folder.
-# For a full list of active aliases, run `alias`.
-#
-# Example aliases
-# alias zshconfig="mate ~/.zshrc"
-# alias ohmyzsh="mate ~/.oh-my-zsh"
 
 
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
